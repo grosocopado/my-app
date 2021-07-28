5b392a48a2226e1d12167750c803b27b9ac42014
pablo1245 <pablo1245@hotmail.com>
2021-07-28 20:01:52 -0300

    my-app.Descripcion: version final del proyecto

---
+++ src/main/java/com/pablo/myapp/entity/TestEntity.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.pablo.myapp.entity;
+
+/**
+ * @author pablo
+ *
+ */
+public class TestEntity {
+	
+	private String nombre;
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+}
